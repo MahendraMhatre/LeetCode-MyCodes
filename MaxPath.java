1477139dd7bad78bafb6e9cdb4c7d5ae1d2a8740
MahendraMhatre <mmhatre@scu.edu>
2017-02-08 00:02:38 -0800

    Maximum path length to the directory - LeetCode

--- MaxPath.java
+++ MaxPath.java
@@ -1,40 +1,46 @@
 import java.util.Stack;
 
 public class MaxPath {
-	int sum, depth;
+	
 	static int max = 0;
 	String name;
 	 public MaxPath() {
-		 this.sum = 0;
-		 this.depth = 0;
 		 name =null; 
 	 }
-	 
-	 
-	 public MaxPath(int sum, int depth, String name) {
+	 public MaxPath( String name) {
 			super();
-			this.sum = sum;
-			this.depth = depth;
 			this.name = name;
 	 }	
 
- 	public int maxLength(String path) {
+ 	public int maxLength(String input) {
  		int count = 0,sum = 0, prev =0;
  		MaxPath mp;
-		String[] parts = path.split("\n");
-		System.out.println(parts[0]);
+		String[] parts = input.split("\n");
+		if(parts.length == 1 && (parts[0].length() - parts[0].replace(".", "").length()) > 0)
+			return parts[0].length();
+			
+		sum = parts[0].length();
 		Stack<MaxPath> st = new Stack<>();
-		mp = new MaxPath(3, 0,parts[0]); 
+		mp = new MaxPath(parts[0]); 
 		st.push(mp);
 		for(int i = 1; i < parts.length; i++) {
 			 
-			if(parts[i].length() - parts[i].replace("\t", "").length() > count) {
+			if(parts[i].length() - parts[i].replace("\t", "").length() > count  ) {
 				count+=1;
-				sum = sum + parts[i].length() ;
-				mp = new MaxPath(sum, count,parts[i]);
+				sum = sum + parts[i].length() - (parts[i].length() - parts[i].replace("\t", "").length() ) + 1;
+				mp = new MaxPath(parts[i]);
 				st.push(mp);
 				if(parts[i].contains(".") && max  < sum) { 
-					 max = sum;	 
+					 max = sum;	
+				 }
+			}
+			else if((parts[i].length() - parts[i].replace(" ", "").length()) > count) {
+			    count+=1;
+				sum = sum + parts[i].length() - 4 + 1;
+				mp = new MaxPath(parts[i]);
+				st.push(mp);
+				if(parts[i].contains(".") && max  < sum) { 
+					 max = sum;	
 				 }
 			}
 			else {
@@ -42,35 +48,25 @@ public class MaxPath {
 				while(!st.isEmpty() && prev <= count) {
 					mp = st.pop();
 					count-=1;
-					sum = sum  - mp.name.length();
-				}
+					sum = sum  - mp.name.length() + (mp.name.length() - mp.name.replace("\t", "").length() ) -1;
+				}//while
 				count+=1;
-				sum = sum + parts[i].length() ;
-				mp = new MaxPath(sum, count,parts[i]);
+				sum = sum + parts[i].length() - (parts[i].length() - parts[i].replace("\t", "").length() ) + 1 ;
+				mp = new MaxPath(parts[i]);
 				st.push(mp);
+				if(parts[i].contains(".") && max  < sum) { 
+					 max = sum;	
+				 }
 			}
 		 }
-		
-		/*if(st.size() <= 1)
-			return 0;
-		while(!st.isEmpty()) {
-			mp = st.pop();
-			if( (mp.name.length() - mp.name.replace("\t", "").length()) <= 0)
-				sum = sum + mp.name.length();
-			else 
-			 sum = sum + mp.name.length() -(mp.name.length() - mp.name.replace("\t", "").length()) + 1 ;
-				
-				
-		}*/
 	
-		
-    return max ;
+    return max;
  	}
 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String path = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext";
+		String path = "dir\n\ta\n\tfile.ext";
 		MaxPath pm = new MaxPath();
 		System.out.println(pm.maxLength(path));
 
